f5c27a1f0f268b525c2cbbb92209764dddcf249a
Miguel Ramos <return.infox@gmail.com>
2023-01-10 18:19:10 -0300

    renderizando opciones del navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  public tituloNav: string = 'Aplicación Angular';
+
+  public opciones: any [] = [
+    { path: '/home', name: 'Home'},
+    { path: '/depositos', name: 'Opciones'},
+    { path: '/prestamos', name: 'Prestamos'},
+    { path: '/giros', name: 'Giros'},
+    { path: '/cta-corriente', name: 'Cuenta Corriente'}
+  ];
 }
+
+// const servicio = { 
+//   path: '/home', 
+//   name: 'Home'
+// }
+
+// servicio.name; // 'Home'
